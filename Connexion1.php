daab4409d13516feaa5508674d6623d770e7d37b
XIANG YU ZHANG <heloise.zhang@edu.ece.fr>
2018-05-02 22:48:03 +0200

    modification du fichier pour qu'il soit utiliser par Connexion2.html

--- Connexion1.php
+++ Connexion1.php
@@ -1,26 +1,29 @@
 <?php
     $email = $_REQUEST["q"];
     $password = $_REQUEST["p"];
+    $res = 0;
 	$db_handle=mysqli_connect("localhost","root","root");
 	$db_found=mysqli_select_db($db_handle,"LinkedInECE");
+    
     if ($db_found)
 	{	
         
 		$sql="SELECT password FROM utilisateur WHERE email='".$email."'";
 		$result = mysqli_query($db_handle,$sql);
-        if (mysql_num_rows($result)>1){echo "Cet email est déjà utilisé par un autre utilisateur, veuillez entrez un autre email";};
+        if (mysqli_num_rows($result)==0){
+            echo "Cet email est incorrect, veuillez entrez un autre email";
+        }
         else {
             while($row = mysqli_fetch_array($result)) {
-                if ($row[0]==password){
-                    echo "mot de passe correct";
-                }
-                else{
+                if ($row[0]!=$password){
                     echo "mot de passe incorrect";
+                }
+                else {
+                    echo "mot de passe correct"; 
                 }
             }
         }
     }
 	else {echo "Base de données non trouvées";}
-
     mysqli_close($db_handle);
 ?>
